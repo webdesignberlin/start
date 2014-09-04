7242da710afd43706b196ba6c0cb9cb50bea667a
Michael Gerstmann <mg@bytepark.de>
2014-09-04 14:27:41 +0200

    add template php

---
+++ web/index.html.php
@@ -0,0 +1,23 @@
+<header class="page-header">
+    <h1 class="page-header__headline">Headline</h1>
+    <p class="page-header__description">Description</p>
+</header>
+
+<main class="page-main">
+    <article class="post">
+        <header class="post__header">
+            <h2></h2>
+        </header>
+        <footer class="post__footer">
+
+        </footer>
+    </article>
+
+    <aside class="aside">
+
+    </aside>
+</main>
+
+<footer class="page-footer">
+    <p class="page-footer__copy">@ Copyright</p>
+</footer>
